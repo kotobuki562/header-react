e85c64542b849c2d151cf51225a653d7544f5ed0
MaQueen <shusineko@icloud.com>
2020-11-05 17:13:16 +0900

    logo edit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,30 @@
 import React from "react";
+import logo from "./logo.svg";
 
 const Navbar = () => {
   return (
     <nav>
-      <h4>navbar</h4>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="links-container">
+          <ul className="links">
+            <li>
+              <a>home</a>
+            </li>
+            <li>
+              <a>home</a>
+            </li>
+            <li>
+              <a>home</a>
+            </li>
+            <li>
+              <a>home</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
