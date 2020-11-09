3371db6f60d6a76c5bccb3beffdb5f2b00105801
MaQueen <shusineko@icloud.com>
2020-11-09 15:56:42 +0900

    commit

--- src/data.js
+++ src/data.js
@@ -1,52 +1,52 @@
-import React from 'react'
-import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import React from "react";
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 export const links = [
   {
     id: 1,
-    url: '/',
-    text: 'random',
+    url: "http://54.248.146.129/",
+    text: "furima",
   },
   {
     id: 2,
-    url: '/about',
-    text: 'about',
+    url: "http://3.218.222.1/",
+    text: "MooVooK",
   },
   {
     id: 3,
-    url: '/projects',
-    text: 'projects',
+    url: "https://kotobuki562.github.io/color-pick/",
+    text: "colorPicker",
   },
-  {
-    id: 4,
-    url: '/contact',
-    text: 'contact',
-  },
-  {
-    id: 5,
-    url: '/profile',
-    text: 'profile',
-  },
-]
+  // {
+  //   id: 4,
+  //   url: "/contact",
+  //   text: "contact",
+  // },
+  // {
+  //   id: 5,
+  //   url: "/profile",
+  //   text: "profile",
+  // },
+];
 
 export const social = [
   {
     id: 1,
-    url: 'https://www.twitter.com',
+    url: "https://www.twitter.com",
     icon: <FaFacebook />,
   },
   {
     id: 2,
-    url: 'https://www.twitter.com',
+    url: "https://www.twitter.com",
     icon: <FaTwitter />,
   },
   {
     id: 3,
-    url: 'https://www.twitter.com',
+    url: "https://www.twitter.com",
     icon: <FaLinkedin />,
   },
   {
     id: 4,
-    url: 'https://www.twitter.com',
+    url: "https://www.twitter.com",
     icon: <FaBehance />,
   },
-]
+];
